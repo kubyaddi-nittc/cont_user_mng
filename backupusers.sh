05d2c7d4d321ba95e8e3419a603e3dbf5f042731
kubyaddi-nittc <keiu@tomakomai-ct.ac.jp>
2022-05-24 10:26:24 +0900

    fix:.gitignore. fix variables. shorten command

--- backupusers.sh
+++ backupusers.sh
@@ -4,7 +4,7 @@ CONT=$1
 BK=backup
 
 mkdir -p $BK
-for $f in passwd shadow group gshadow setuid setgid; do
-  docker cp -a $CONT:/etc/$f $BK;
+for f in passwd shadow group gshadow setuid setgid; do
+  docker cp -a $CONT:/etc/$f $BK
 done
 
