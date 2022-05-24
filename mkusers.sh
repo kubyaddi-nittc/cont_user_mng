dc926a497d466b19fca464397c2eaf1cdc5dce1c
kubyaddi-nittc <keiu@tomakomai-ct.ac.jp>
2022-05-24 09:55:07 +0900

    add echo progress. fix copying process of /etc/skel. shorten command.

--- mkusers.sh
+++ mkusers.sh
@@ -3,13 +3,18 @@
 CONT=$1
 USERS=users.txt
 
+echo 'Create users'
 docker cp $USERS $CONT:/
 docker exec $CONT newusers /$USERS
-for user in `cut -d: -f1 $USERS`; do
-  HM=`cat $USERS | grep $user | cut -d: -f6`
-  echo $HM
-  docker exec $CONT cp -a /etc/skel $HM
-  docker exec $CONT chown -R $user $HM
-  docker exec $CONT chgrp -R `cut $USERS | grep $user | cut -d: -f4` $HM
-done
 docker exec $CONT rm /$USERS
+echo 'Create users home dir'
+docker cp -a $CONT:/etc/skel ./skel
+for user in `cut -d: -f1 $USERS`; do
+  echo $user
+  HM=`grep ^$user $USERS | cut -d: -f6`
+  docker exec $CONT rm -rf $HM
+  docker cp -a ./skel $CONT:$HM
+  docker exec $CONT chown -R $user:`grep ^$user $USERS | cut -d: -f4` $HM
+done
+rm -rf ./skel
+echo 'All done!'
