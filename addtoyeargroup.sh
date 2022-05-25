4351d4ef9e73c9c0ecc4e7e18c2f962b12fc7d28
Keiu Harada <keiu@tomakomai-ct.ac.jp>
2022-05-25 13:26:14 +0900

    - Change argument order. - Remove unnecessary command. - Enable script executable permission.

--- addtoyeargroup.sh
+++ addtoyeargroup.sh
@@ -1,13 +1,11 @@
 #/bin/bash
 
 CONT=$1
-GROUP=$2
-GID=$3
+GID=$2
+GROUP=$3
 USERS=users.txt
 
-docker cp $USERS $CONT:/
 docker exec $CONT groupadd -g $GID $GROUP
 for user in `cut -d: -f1 $USERS`; do
   docker exec $CONT usermod -a -G $GROUP
 done
-docker exec $CONT rm /$USERS
